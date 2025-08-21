66e845004c6505328bd1912fd3740a896bb89b23
makachanm <hyeonseongyang09@gmail.com>
2025-08-21 21:39:50 +0900

    (edit): gitignore and test script

--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@
 
 export MASTODON_INSTANCE_URL="your.mastodon.instance"
 export MASTODON_TOKEN="your_access_token"
-export SERVICE_URL="your.questionbasket.service"
+export SERVICE_URL="localhost"
+export DATABASE_URL="data.db"
 
 go build
 ./questionbasket
